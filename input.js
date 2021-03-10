00758f72ea785bd23bac76e48a50d75cb1da691c
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-10 22:08:38 +0000

    refactored setupInput function to be self contained in input.js

---
+++ input.js
@@ -0,0 +1,21 @@
+const handleUserInput = key => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+module.exports = { setupInput };
