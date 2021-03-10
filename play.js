f3437a2edbd60372f46cde7d9f109a76b9edfbf1
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-10 22:00:53 +0000

    created handleUserInput and setupInput function for game to accept standard input

--- play.js
+++ play.js
@@ -1,3 +1,24 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = key => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
